dcd00019a7e18b725d64f7e86a887740cabc1380
username <596061612@qq.com>
2020-03-15 12:26:58 +0800

    把本地工程添加到commit

---
+++ com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git projest");
+        System.out.println("hello git projest");
+        System.out.println("hello git projest");
+        System.out.println("hello git projest");
+    }
+}
